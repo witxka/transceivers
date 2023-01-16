334894e21424a07f022164cd51b8383b84d81969
Victor Kotlyar <witxka@gmail.com>
2023-01-16 22:36:51 +0300

    Fix typo in file name

--- copy-to-target.sh
+++ copy-to-target.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cp transceiversr.py ~/local/lib/check_mk/base/plugins/agent_based/
+cp transceivers.py ~/local/lib/check_mk/base/plugins/agent_based/
 cp package-info.py ~/var/check_mk/packages/transceivers
 cp agent/transceivers ~/local/share/check_mk/agents/plugins/
